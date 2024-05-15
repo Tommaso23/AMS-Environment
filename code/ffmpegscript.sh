8005a6eef019961a1b5d99c13260a6c560657c33
Tommaso23 <tombucaioni@gmail.com>
2024-05-15 10:26:26 +0200

    modify script and zip file

--- code/ffmpegscript.sh
+++ code/ffmpegscript.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # Check if two arguments (ffmpeg command and output directory) are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <ffmpeg_command> <output_directory>"
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <ffmpeg_command> <output_directory> <api_url>"
     exit 1
 fi
+
 
 # Get the input and output file paths, and the output directory from the arguments
 FFMPEG_COMMAND="$1"
 OUTPUT_DIR="$2"
-CHANGE_STATUS_API="$3"
+API_URL="$3"
+
+echo $API_URL
 
 # Create the output directory if it doesn't exist
 mkdir -p "/mnt/azfile/$OUTPUT_DIR/output"
@@ -25,11 +28,9 @@ JSON_DATA="{\"exitStatus\": $EXIT_STATUS, \"jobId\": \"$OUTPUT_DIR\"}"
 
 # Check if the ffmpeg command succeeded
 if [ $EXIT_STATUS -eq 0 ]; then
-	curl -X POST -H "Content-Type: application/json" -d "$JSON_DATA" "$CHANGE_STATUS_API"
-
-	# curl -X POST -H "Content-Type: application/json" -d "$JSON_DATA" https://tom-encoder-fa-api.azurewebsites.net/api/encoderJobs/changeStatus?clientId=blobs_extension
+	curl -X POST -H "Content-Type: application/json" -d "$JSON_DATA" "$API_URL"
     echo "Encoding completed successfully!"
 else
-	curl -X POST -H "Content-Type: application/json" -d "$JSON_DATA" https://tom-encoder-fa-api.azurewebsites.net/api/encoderJobs/changeStatus?clientId=blobs_extension
+	curl -X POST -H "Content-Type: application/json" -d "$JSON_DATA" "$API_URL"
     echo "Error during encoding. Exit status: $EXIT_STATUS"
 fi
